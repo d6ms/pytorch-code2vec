d3785767235f76479e14ff86dad1b8af79962b23
d6ms <g.denden.ms@gmail.com>
2020-08-07 17:37:56 +0900

    rename

--- models.py
+++ models.py
@@ -5,44 +5,45 @@ from torch.nn import functional as F
 
 class Code2Vec(nn.Module):
 
-    def __init__(self, nodes_dim, paths_dim, embedding_dim, output_dim, dropout):
+    def __init__(self, words_dim, paths_dim, embedding_dim, output_dim, dropout):
         super(Code2Vec, self).__init__()
-        self.node_embedding = nn.Embedding(nodes_dim, embedding_dim)
+        self.node_embedding = nn.Embedding(words_dim, embedding_dim)
         self.path_embedding = nn.Embedding(paths_dim, embedding_dim)
         self.W = nn.Parameter(torch.randn(1, embedding_dim, 3*embedding_dim))
         self.a = nn.Parameter(torch.randn(1, embedding_dim, 1))
         self.out = nn.Linear(embedding_dim, output_dim)
         self.dropout = nn.Dropout(dropout)
 
-    def forward(self, starts, paths, ends):
-        #starts = paths = ends = [batch size, max length]
+    def forward(self, x_s, path, x_t):
+        # x_s = path = x_t = [batch size, max length]
+        batch_size = x_s.shape[0]
         
         # Embedding した結果のベクトルを concatenate
-        embedded_starts = self.node_embedding(starts)
-        embedded_paths = self.path_embedding(paths)
-        embedded_ends = self.node_embedding(ends)
+        embedded_x_s = self.node_embedding(x_s)
+        embedded_path = self.path_embedding(path)
+        embedded_x_t = self.node_embedding(x_t)
         # embedded_* = [batch size, max length, embedding dim]
-        c = self.dropout(torch.cat((embedded_starts, embedded_paths, embedded_ends), dim=2))
+        c = self.dropout(torch.cat((embedded_x_s, embedded_path, embedded_x_t), dim=2))
         # c = [batch size, max length, embedding dim * 3]
 
         # Fully Connected レイヤを通して combined context vector を計算
-        W = self.W.repeat(starts.shape[0], 1, 1)  # batch_size 分だけ複製する
+        W = self.W.repeat(batch_size, 1, 1)  # batch_size 分だけ複製する
         # W = [batch size, embedding dim, embedding dim * 3]
         c = c.permute(0, 2, 1)  # 積の計算順序の都合上、テンソルの次元を入れ替えてから計算して戻す
-        x = torch.tanh(torch.bmm(W, c))
-        x = x.permute(0, 2, 1)
-        # x = [batch size, max length, embedding dim]
+        c = torch.tanh(torch.bmm(W, c))
+        c = c.permute(0, 2, 1)
+        # c = [batch size, max length, embedding dim]
 
         # Attention weight の計算
-        a = self.a.repeat(starts.shape[0], 1, 1)  # batch_size 分だけ複製する
+        a = self.a.repeat(batch_size, 1, 1)  # batch_size 分だけ複製する
         # a = [batch size, embedding dim, 1]
-        z = F.softmax(torch.bmm(x, a).squeeze(2), dim=1).unsqueeze(2)
-        # z = [batch size, max length, 1]
+        a = F.softmax(torch.bmm(c, a).squeeze(2), dim=1).unsqueeze(2)
+        # a = [batch size, max length, 1]
 
         # Code vector の計算
-        x = x.permute(0, 2, 1)
-        # x = [batch size, embedding dim, max length]
-        v = torch.bmm(x, z).squeeze(2)
+        c = c.permute(0, 2, 1)
+        # c = [batch size, embedding dim, max length]
+        v = torch.bmm(c, a).squeeze(2)
         # v = [batch size, embedding dim]
 
         # ここまでで code vector (v) が計算できた
